eb691d74dedcc45dfc3bd2329df0499e9ad80046
lgazzaneo <lgazzaneo@itba.edu.ar>
2024-11-24 20:48:22 -0300

    ignore commit

--- VAEAutoencoder.py
+++ VAEAutoencoder.py
@@ -49,6 +49,7 @@ class VAEAutoencoder:
                 # Initialize weights randomly from a normal distribution
                 self.weights[i] = np.random.randn(self.total_layers[i], self.total_layers[i + 1]) / 3
         self.prev_weight_updates = [np.zeros_like(w) for w in self.weights]
+        
         return self.weights
     
     def forward_propagation(self, x, weights=None):
@@ -65,6 +66,7 @@ class VAEAutoencoder:
     
     def encode(self, x_input):
         encoder_weights = self.weights[: len(self.total_layers) // 2]
+        print(encoder_weights)
         activations, excitations = self.forward_propagation(x_input, encoder_weights)
         print(excitations)
         print(activations)
@@ -76,7 +78,7 @@ class VAEAutoencoder:
         epsilon = np.random.normal(0,1)
         Z = []
         for x,y in aux:
-            Z.append(np.exp(0.5 * x) * epsilon + y)
+            Z.append(x * epsilon + y)
         print(Z)
         return Z, epsilon
     
@@ -169,6 +171,7 @@ class VAEAutoencoder:
         return weight_updates
     
     def encoder_backprop(self, Z_grad, epsilon, activations, excitations):
+        encoder_weights = self.weights[: len(self.total_layers) // 2]
         array = activations[-1]
         aux = [(array[i], array[i + 1]) for i in range(0, len(array), 2)]
         latent_gradients = []
@@ -177,7 +180,8 @@ class VAEAutoencoder:
             log_var = sigma * np.exp(0.5 * aux[i][0]) * 0.5 - 0.5 * (1 - np.exp(aux[i][0]))
             mu = Z_grad[i] + aux[i][1]
             latent_gradients.append(np.array(mu, log_var))
-
+        
+        
         
         
         
@@ -189,9 +193,9 @@ if __name__ == '__main__':
     # Create a deep autoencoder with a mirrored architecture
     # Encoder layers: 2 -> 1 
     # Decoder layers: 1 -> 2 (automatically mirrored)
-    autoencoder = VAEAutoencoder(encoder_layers=[2, 2], learning_rate=0.1, momentum=0.0)
+    autoencoder = VAEAutoencoder(encoder_layers=[2, 3, 1], learning_rate=0.1, momentum=0.0)
     print(autoencoder.initialize_weights())
-    latent = autoencoder.encode(X[0])
-    Z = autoencoder.reparametrization_trick(latent)
-    out = autoencoder.decode(Z)
+    a, b = autoencoder.encode(X[0])
+    Z, epsilon = autoencoder.reparametrization_trick()
+
     
